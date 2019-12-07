5422656c7258a89ec40c1c13f3d45327940ced84
Gregory Burke <gregoryburke@Gregorys-MBP.wp.comcast.net>
2019-12-07 16:24:40 -0500

    created game logic

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
 
 
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds"
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {console.log("You found a match!");
@@ -11,10 +32,12 @@ function checkForMatch() {
 function flipCard(cardId) {
 	flipCard([0]);
 	flipCard([2]);
-console.log("User flipped" + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
+console.log("User flipped" + cards[cardId].rank);
+cardsInPlay.push(cards["queen"].rank);
+cardsInPlay.push(cards["king"].rank);
+console.log(cardImage);
+console.log(suit)
 checkForMatch();
-}
 };
 
 
