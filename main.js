9765ea25a25f2d4f306f81e79650d5083e3cb8a2
Gregory Burke <gregoryburke@Gregorys-MBP.wp.comcast.net>
2019-12-07 13:40:31 -0500

    created game logic

--- main.js
+++ main.js
@@ -2,18 +2,19 @@
 
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-
-var cardOne = cards[1];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {alert("You found a match!");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {console.log("You found a match!");
 } else {
-	alert("Sorry, try again");
+	console.log("Sorry, try again");
+}
+}
+function flipCard(cardId) {
+	flipCard([0]);
+	flipCard([2]);
+console.log("User flipped" + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+}
 };
 
 
